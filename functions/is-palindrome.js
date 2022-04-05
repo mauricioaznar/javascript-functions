b92523afbc65d4f5e324a22baaf557c3d0bfe77e
mauricioaznar <maurisio01>
2022-04-05 16:35:07 -0500

    Termine la funcion de palindromo. ya dice si es o no es

--- functions/is-palindrome.js
+++ functions/is-palindrome.js
@@ -4,7 +4,16 @@ function isPair(str) {
 
 
 function isPalindrome(str) {
-    return false
+    const original = str.split("")
+    const l = original.length
+    let wordBackwards = []
+    for(let i = 0; i < l; i++){
+        let letter = original.pop()
+        wordBackwards.push(letter)
+    }
+    wordBackwards = wordBackwards.toString()
+    console.log(wordBackwards===str.split('').toString())
+    return wordBackwards===str.split('').toString()
 }
 
 module.exports = {
