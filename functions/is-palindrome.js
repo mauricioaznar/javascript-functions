0a70c46fe76f9fc3c017989c93241e3dd5a19d45
mauricioaznar <maurisio01>
2022-04-05 16:37:21 -0500

    Termine la funcion de palindromo. ya dice si es o no es (simplifique un poco la funcion)

--- functions/is-palindrome.js
+++ functions/is-palindrome.js
@@ -11,9 +11,7 @@ function isPalindrome(str) {
         let letter = original.pop()
         wordBackwards.push(letter)
     }
-    wordBackwards = wordBackwards.toString()
-    console.log(wordBackwards===str.split('').toString())
-    return wordBackwards===str.split('').toString()
+    return wordBackwards.toString() === str.split('').toString()
 }
 
 module.exports = {
